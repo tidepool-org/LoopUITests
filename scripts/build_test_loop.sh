54a7d9491683cc51f6f777f190f1d078ec299a0b
Jamie <jamie.h.bate@gmail.com>
2019-12-13 10:47:50 +1300

    remove run_f

--- scripts/build_test_loop.sh
+++ scripts/build_test_loop.sh
@@ -3,6 +3,6 @@
 BUILD_DIR="$1"
 
 pushd $BUILD_DIR
-run_f "detox build --configuration ios.sim.release"
-#run_f "detox test --configuration ios.sim.release --cleanup"
+detox build --configuration ios.sim.release
+#detox test --configuration ios.sim.release --cleanup
 popd
