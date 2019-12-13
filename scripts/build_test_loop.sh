7da518e4eb6ff775202aed7a695bd525f33ca981
Jamie <jamie.h.bate@gmail.com>
2019-12-13 14:23:26 +1300

    build in bash script

--- scripts/build_test_loop.sh
+++ scripts/build_test_loop.sh
@@ -2,8 +2,8 @@
 
 rm -rf build
 mkdir build
-cd  build && \
-git clone --branch=darinkrauss/dev --recurse-submodules https://github.com/tidepool-org/LoopWorkspace && \
+cd  build
+git clone --branch=darinkrauss/dev --recurse-submodules https://github.com/tidepool-org/LoopWorkspace
 xcodebuild -workspace ./LoopWorkspace/Loop.xcworkspace -scheme 'Loop (Workspace)' -configuration Debug -destination 'name=iPhone 8' build SYMROOT="$(pwd)"
 
 #detox build --configuration ios.sim.release
