f9e6a64cbb36f0d060df9af124a906ae35596327
Jamie <jamie.h.bate@gmail.com>
2020-01-22 12:11:00 +1300

    wip for removing critical-alerts

--- Makefile
+++ Makefile
@@ -1,13 +1,5 @@
 debug-build:
-	rm -rf build
-	mkdir build
-	cd  build && \
-	git clone --branch=darinkrauss/dev --recurse-submodules https://github.com/tidepool-org/LoopWorkspace && \
-	xcodebuild -workspace ./LoopWorkspace/Loop.xcworkspace -scheme 'Loop (Workspace)' -configuration Debug -destination 'name=iPhone 8' build SYMROOT="$(pwd)"
+	. scripts/build_loop.dev.sh Debug debug_build_log.txt darinkrauss/dev
 
 release-build:
-	rm -rf build
-	mkdir build
-	cd build && \
-	git clone --branch=master --recurse-submodules https://github.com/tidepool-org/LoopWorkspace && \
-	xcodebuild -workspace ./LoopWorkspace/Loop.xcworkspace -scheme 'Loop (Workspace)' -configuration Release -destination 'name=iPhone 8' build SYMROOT="$(pwd)"
+	. scripts/build_loop.dev.sh Release release_build_log.txt
--- scripts/build_loop.dev.sh
+++ scripts/build_loop.dev.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
+buildType="Debug"
+buildLog="/dev/null 2>&1"
+branch="dev"
+
+if [ "$1" != "" ]; then
+    buildType="$1"
+fi
+
+if [ "$2" != "" ]; then
+    buildLog="$2"
+fi
+
+if [ "$3" != "" ]; then
+    branch="$3"
+fi
+
+echo "Building as $buildType build"
+echo "Build logs at $buildLog"
+echo "Build branch $branch"
+
+
+#rm -rf build
+#mkdir build
+#cp $(pwd)/scripts/LoopTestOverride.xcconfig ./build/LoopTestOverride.xcconfig
+cp $(pwd)/scripts/Loop.test.entitlements ./build/LoopWorkspace/Loop/Loop/Loop.entitlements
 cd  build
-git clone --branch=darinkrauss/dev --recurse-submodules https://github.com/tidepool-org/LoopWorkspace >/dev/null 2>&1
-xcodebuild -workspace ./LoopWorkspace/Loop.xcworkspace -scheme 'Loop (Workspace)' -configuration Debug -destination 'name=iPhone 8' build SYMROOT="$(pwd)" >/dev/null 2>&1
+# git clone --branch="$branch" --recurse-submodules https://github.com/tidepool-org/LoopWorkspace >"$buildLog"
+xcodebuild -workspace ./LoopWorkspace/Loop.xcworkspace -scheme 'Loop (Workspace)' -configuration "$buildType" -xcconfig LoopTestOverride.xcconfig -destination 'name=iPhone 11 Pro' build SYMROOT="$(pwd)" >"$buildLog"
