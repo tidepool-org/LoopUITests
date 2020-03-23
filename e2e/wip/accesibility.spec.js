f48e3806d0040d934a7fa338a131b4e6d55e7473
Jamie <jamie.h.bate@gmail.com>
2020-03-24 11:33:01 +1300

    testing overrides screen

--- e2e/wip/accesibility.spec.js
+++ e2e/wip/accesibility.spec.js
@@ -4,18 +4,29 @@ describe('quick test', () => {
     beforeAll(async () => {
         await setup.LaunchLoop();
     });
-    describe('settings', () => {
-        beforeAll(async () => {
-            await screen.settings.Open();
-        });
-        afterAll(async () => {
-            await screen.settings.Close();
-        });
-        it('can be go set to closed loop', async () => {
-            await screen.settings.ClosedLoop();
-        });
-        it('can be go set to open loop', async () => {
-            await screen.settings.OpenLoop();
+    describe('overrides', () => {
+        describe('override', () => {
+            it('can open overrides screen', async () => {
+                await screen.overrides.Open();
+            });
+            // afterAll(async () => {
+            //     await screen.overrides.Cancel();
+            // });
+            it('has a Custom Preset Header', async () => {
+                await expect(screen.overrides.CustomPresetHeader()).toExist();
+            });
+            // it('has a Cancel Header Button', async () => {
+            //     await expect(screen.overrides.CancelHeaderButton()).toExist();
+            // });
+            // it('has a Edit Header Button', async () => {
+            //     await expect(screen.overrides.EditHeaderButton()).toExist();
+            // });
+            // it('has a Add Header Button', async () => {
+            //     await expect(screen.overrides.AddHeaderButton()).toExist();
+            // });
+            // it('has a Add Preset Message', async () => {
+            //     await expect(screen.overrides.AddPresetMessage()).toExist();
+            // });
         });
     });
 });
