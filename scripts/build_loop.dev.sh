2287ef21e84c0a43face58b818b2c76ab658631a
Jamie <jamie.h.bate@gmail.com>
2020-04-02 15:10:50 +1300

    debug build only, cleanup build script

--- scripts/build_loop.dev.sh
+++ scripts/build_loop.dev.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
 buildType="Debug"
-buildLog="/dev/null 2>&1"
-
 branch="dev"
 
 if [ "$1" != "" ]; then
-    buildType="$1"
+    branch="$1"
 fi
 
 if [ "$2" != "" ]; then
-    branch="$2"
+    buildType="$2"
 fi
-
-if [ "$3" != "" ]; then
-    buildLog="$3"
-fi
-
 
 
 echo "Building as $buildType build"
-echo "Build logs at $buildLog"
 echo "Build branch $branch"
 
 rm -rf build
 mkdir build
 cd  build
-git clone --branch="$branch" --recurse-submodules git@github.com:tidepool-org/LoopWorkspace.git > "$buildLog"
+git clone --branch="$branch" --recurse-submodules git@github.com:tidepool-org/LoopWorkspace.git
 
-xcodebuild -workspace ./LoopWorkspace/Tidepool/Tidepool.xcworkspace -scheme 'Tidepool Loop' -configuration "$buildType" -destination 'name=iPhone 11 Pro' build SYMROOT="$(pwd)" >"$buildLog"
+xcodebuild -workspace ./LoopWorkspace/Tidepool/Tidepool.xcworkspace -scheme 'Tidepool Loop' -configuration "$buildType" -destination 'name=iPhone 11 Pro' build SYMROOT="$(pwd)"
