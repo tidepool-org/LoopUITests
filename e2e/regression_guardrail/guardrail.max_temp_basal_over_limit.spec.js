676a97d88e655c8edc15696e14df5c2ebb648b97
Jamie Bate <jamie@tidepool.org>
2020-04-02 16:28:16 +1300

    Sprint13 extras (#14)
    * debug build only, cleanup build script
    
    * flat_cgm_trace_with_basal
    
    * guard-rail max temp basal

---
+++ e2e/regression_guardrail/guardrail.max_temp_basal_over_limit.spec.js
@@ -0,0 +1,16 @@
+const { loop } = require('../../src/index');
+
+describe('max temp basal for corrections is either pump maximum or 35 u/hr if the pump doesn’t have a maximum', () => {
+    it('launch loop', async () => {
+        await loop.Launch();
+    });
+    it('open settings add pump simulator, max basal rate set over 35 units', async () => {
+        await loop.Configure({
+            settings: {
+                AddPumpSimulator: true,
+                DeliveryLimits: { maxBolus: '1.0', maxBasalRate: '45.0' }
+            }
+        });
+    });
+});
+
