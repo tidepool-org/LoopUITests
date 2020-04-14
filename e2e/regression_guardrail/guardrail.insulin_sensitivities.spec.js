a860cc3788af9da01fc64be214e7d4fd2e76fc0e
Jamie <jamie.h.bate@gmail.com>
2020-04-14 17:15:30 +1200

    use standard setup and then one test for each limit being tested

--- e2e/regression_guardrail/guardrail.insulin_sensitivities.spec.js
+++ e2e/regression_guardrail/guardrail.insulin_sensitivities.spec.js
@@ -1,65 +1,49 @@
 const { loop } = require('../../src/index');
 
 describe('guardrail insulin sensitivities', () => {
-    it('cannot set 501 units', async () => {
+
+    beforeAll(async () => {
         await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '501' }],
-            }
-        });
-        //TODO assert cannot be set
+        await loop.Configure({ settings: { AddPumpSimulator: true } });
+    });
+
+    beforeEach(async () => {
+        await loop.screens.settings.Open();
+    });
+
+    afterEach(async () => {
+        await loop.screens.settings.Close();
+    });
+
+    it('cannot set 501 units', async () => {
+        try {
+            await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '501' }]);
+        } catch (error) {
+            //TODO assert cannot be set
+        }
     });
     it('set 500 units, warning', async () => {
-        await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '500' }],
-            }
-        });
+        await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '500' }]);
         //TODO assert on warning
     });
     it('set 400 units, warning', async () => {
-        await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '400' }],
-            }
-        });
+        await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '400' }]);
         //TODO assert on warning
     });
     it('set 15 units, warning', async () => {
-        await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '15' }],
-            }
-        });
+        await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '15' }]);
         //TODO assert on warning
     });
     it('set 10 units, warning', async () => {
-        await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '10' }],
-            }
-        });
+        await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '10' }]);
         //TODO assert on warning
     });
     it('cannot set 9 units', async () => {
-        await loop.Launch();
-        await loop.Configure({
-            settings: {
-                AddPumpSimulator: true,
-                InsulinSensitivities: [{ time: '12:00 AM', bgValuePerInsulinUnit: '9' }],
-            }
-        });
-        //TODO assert cannot be set
+        try {
+            await loop.screens.settings.SetInsulinSensitivities([{ time: '12:00 AM', bgValuePerInsulinUnit: '9' }]);
+        } catch (error) {
+            //TODO assert cannot be set
+        }
     });
 });
 
