2cf0e171bc728cd809b540ac19c0d490409a8173
Jamie <jamie.h.bate@gmail.com>
2020-04-16 12:14:53 +1200

    cleanup un-used config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,11 @@
 
 const basalRatesUnits = 'U/hr';
-const basalRatesUnitsPerHourStepSize = 0.05;
 const insulinSensitivitiesUnits = 'mg/dL/U';
-const insulinSensitivitiesMaximum = 500;
 const correctionRangesMaximum = 180;
 
 var config = {
     basalRatesUnits,
-    basalRatesUnitsPerHourStepSize,
     insulinSensitivitiesUnits,
-    insulinSensitivitiesMaximum,
     correctionRangesMaximum
 };
 
