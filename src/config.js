ac7d4b23747a4eb843224a6105c9c659c7dc53aa
Jamie <jamie.h.bate@gmail.com>
2020-05-14 15:56:35 +1200

    remove unwanted changes

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
-
 const basalRatesUnits = 'U/hr';
 const insulinSensitivitiesUnits = 'mg/dL/U';
 const correctionRangesMaximum = 180;
-
-const units = {
-    mgdL: 'mg/dL',
-    mmolL: 'mmol/L',
-};
 
 var config = {
     basalRatesUnits,
