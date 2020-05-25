4392e840b1adfcbc4ce3544e6c9376cfc8952cf8
Jamie <jamie.h.bate@gmail.com>
2020-05-25 15:59:27 +1200

    changes while testing

--- e2e/functional/functional_test.spec.js
+++ e2e/functional/functional_test.spec.js
@@ -9,31 +9,27 @@ describe('functional test', () => {
         await settings.AddPumpSimulator();
     });
     describe('guardrails', () => {
-        // describe('max bolus', () => {
-        //     guardrailsTests.maxBolusTests(test);
-        // });
-        // describe('basal rate schedule', () => {
-        //     guardrailsTests.basalRateScheduleTests(test);
-        // });
+        describe('max bolus', () => {
+            guardrailsTests.maxBolusTests(test);
+        });
+        describe('basal rate schedule', () => {
+            guardrailsTests.basalRateScheduleTests(test);
+        });
         describe('insulin sensitivity schedule', () => {
             guardrailsTests.insulinSensitivityScheduleTests(test);
         });
-        // describe('max temp basal rate', () => {
-        //     guardrailsTests.maxTempBasalRateTests(test);
-        // });
-        // describe('suspend threshold', () => {
-        //     guardrailsTests.suspendThresholdTests(test);
-        // });
-        //TODO: update when development work complete
-        describe.skip('correction range schedule', () => {
+        describe('max temp basal rate', () => {
+            guardrailsTests.maxTempBasalRateTests(test);
+        });
+        describe('suspend threshold', () => {
+            guardrailsTests.suspendThresholdTests(test);
+        });
+        describe('correction range schedule', () => {
             guardrailsTests.correctionRangeScheduleTests(test);
         });
         //TODO: update when development work complete
         describe.skip('insulin carb ratio', () => {
             guardrailsTests.insulinCarbRatioTests(test);
-        });
-        describe.skip('insulin sensitivity schedule', () => {
-            guardrailsTests.insulinSensitivityScheduleTests(test);
         });
     });
 });
