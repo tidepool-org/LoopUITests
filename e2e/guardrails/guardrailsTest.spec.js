f6ea3c9f8deec2fafd0b2694d39d70270d6b130c
Jamie <jamie.h.bate@gmail.com>
2020-06-29 17:17:56 +1200

    guard-rails tests running

--- e2e/guardrails/guardrailsTest.spec.js
+++ e2e/guardrails/guardrailsTest.spec.js
@@ -13,28 +13,28 @@ describe('guardrails test', () => {
         let settings = await test.OpenSettingsScreen();
         await settings.AddPumpSimulator();
     });
-    //TODO: add back when guardrails work is complete
-    describe.skip('max bolus', () => {
-        guardrailsTests.maxBolusTests(test);
-    });
     describe('insulin sensitivity schedule', () => {
         guardrailsTests.insulinSensitivityScheduleTests(test);
+    });
+    describe('suspend threshold', () => {
+        guardrailsTests.suspendThresholdTests(test);
+    });
+    describe('insulin carb ratio', () => {
+        guardrailsTests.insulinCarbRatioTests(test);
+    });
+    describe('correction range schedule', () => {
+        guardrailsTests.correctionRangeScheduleTests(test);
+    });
+    //TODO: add back when guardrails work is complete
+    describe.skip('basal rate schedule', () => {
+        guardrailsTests.basalRateScheduleTests(test);
     });
     //TODO: add back when guardrails work is complete
     describe.skip('max temp basal rate', () => {
         guardrailsTests.maxTempBasalRateTests(test);
     });
-    describe('suspend threshold', () => {
-        guardrailsTests.suspendThresholdTests(test);
-    });
-    describe('correction range schedule', () => {
-        guardrailsTests.correctionRangeScheduleTests(test);
-    });
-    describe('insulin carb ratio', () => {
-        guardrailsTests.insulinCarbRatioTests(test);
-    });
     //TODO: add back when guardrails work is complete
-    describe.skip('basal rate schedule', () => {
-        guardrailsTests.basalRateScheduleTests(test);
+    describe.skip('max bolus', () => {
+        guardrailsTests.maxBolusTests(test);
     });
 });
