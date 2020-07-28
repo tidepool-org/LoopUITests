891d6765b10dc61efc5b35dba12b59d4abc59247
Jamie <jamie.h.bate@gmail.com>
2020-07-28 14:04:19 +1200

    better assertions

--- tests/functionality/pumpSimulatorScreen.tests.js
+++ tests/functionality/pumpSimulatorScreen.tests.js
@@ -12,27 +12,35 @@ var pumpSimulatorScreen = (test) => {
     });
     it('set errorOnBolus to true', async () => {
         await pumpSimulatorScreen.Apply({ errorOnBolus: true });
+        await expect(pumpSimulatorScreen.ErrorOnBolusToggel()).toHaveValue('1');
     });
     it('set errorOnBolus to false', async () => {
         await pumpSimulatorScreen.Apply({ errorOnBolus: false });
+        await expect(pumpSimulatorScreen.ErrorOnBolusToggel()).toHaveValue('0');
     });
     it('set errorOnTempBasal to true', async () => {
         await pumpSimulatorScreen.Apply({ errorOnTempBasal: true });
+        await expect(pumpSimulatorScreen.ErrorOnTempBasalToggel()).toHaveValue('1');
     });
     it('set errorOnTempBasal to false', async () => {
         await pumpSimulatorScreen.Apply({ errorOnTempBasal: false });
+        await expect(pumpSimulatorScreen.ErrorOnTempBasalToggel()).toHaveValue('0');
     });
     it('set errorOnSuspend to true', async () => {
         await pumpSimulatorScreen.Apply({ errorOnSuspend: true });
+        await expect(pumpSimulatorScreen.ErrorOnSuspendToggel()).toHaveValue('1');
     });
     it('set errorOnSuspend to false', async () => {
         await pumpSimulatorScreen.Apply({ errorOnSuspend: false });
+        await expect(pumpSimulatorScreen.ErrorOnSuspendToggel()).toHaveValue('0');
     });
     it('set errorOnResume to true', async () => {
         await pumpSimulatorScreen.Apply({ errorOnResume: true });
+        await expect(pumpSimulatorScreen.ErrorOnResumeToggel()).toHaveValue('1');
     });
     it('set errorOnResume to false', async () => {
         await pumpSimulatorScreen.Apply({ errorOnResume: false });
+        await expect(pumpSimulatorScreen.ErrorOnResumeToggel()).toHaveValue('0');
     });
     it('set battery percent', async () => {
         await pumpSimulatorScreen.Apply({ batteryRemaining: 80 });
