47dbc69fccedeee8428c54c967fde00e483ebea7
Jamie <jamie.h.bate@gmail.com>
2020-08-12 16:27:04 +1200

    try again

--- scripts/test.sh
+++ scripts/test.sh
@@ -41,7 +41,7 @@ if [ ${#} -ne 0 ]; then
   error "Unexpected arguments: ${*}"
 fi
 
-if [ "${TEST_TYPE}" != *"functional_"* -a "${TEST_TYPE}" != *"smoke_"* -a  "${TEST_TYPE}" != *"guardrails_"* ]; then
+if [[ "${TEST_TYPE}" != *"functional_"* -a "${TEST_TYPE}" != *"smoke_"* -a  "${TEST_TYPE}" != *"guardrails_"* ]]; then
   error "Unexpected test type: ${TEST_TYPE}"
 fi
 
