31399ed13ccd0d95e2ed8b1e2146af81cbee1fd5
Jamie <jamie.h.bate@gmail.com>
2020-08-31 10:59:41 +1200

    remove debug statements

--- scripts/load_scenarios.sh
+++ scripts/load_scenarios.sh
@@ -6,16 +6,10 @@ if [ "$1" != "" ]; then
     deviceId="$1"
 fi
 
-echo "device ID $deviceId"
-
 appDir=~/Library/Developer/CoreSimulator/Devices/$1/data/Containers/Data/Application
-
-echo "app dir $appDir"
 
 cd $appDir
 applicationId="$(ls -t | head -n1)"
-
-echo "app ID $applicationId"
 
 cd $workingDir
 
