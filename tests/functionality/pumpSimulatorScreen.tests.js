0ea9e6cd85af6f243e4456cf104722b9fdb77d23
Jamie <jamie.h.bate@gmail.com>
2020-09-15 15:51:31 +1200

    group tests together

--- tests/functionality/pumpSimulatorScreen.tests.js
+++ tests/functionality/pumpSimulatorScreen.tests.js
@@ -6,35 +6,27 @@ module.exports = (test) => {
     it('open simulator', async () => {
         screen = await test.openPumpScreen();
     });
-    it('set errorOnBolus to true', async () => {
+    it('switch errorOnBolus on and off', async () => {
         await screen.Apply({ errorOnBolus: true });
         await expect(screen.ErrorOnBolusSwitch()).toHaveValue('1');
-    });
-    it('set errorOnBolus to false', async () => {
         await screen.Apply({ errorOnBolus: false });
         await expect(screen.ErrorOnBolusSwitch()).toHaveValue('0');
     });
-    it('set errorOnTempBasal to true', async () => {
+    it('switch errorOnTempBasal on and off', async () => {
         await screen.Apply({ errorOnTempBasal: true });
         await expect(screen.ErrorOnTempBasalSwitch()).toHaveValue('1');
-    });
-    it('set errorOnTempBasal to false', async () => {
         await screen.Apply({ errorOnTempBasal: false });
         await expect(screen.ErrorOnTempBasalSwitch()).toHaveValue('0');
     });
-    it('set errorOnSuspend to true', async () => {
+    it('switch errorOnSuspend on and off', async () => {
         await screen.Apply({ errorOnSuspend: true });
         await expect(screen.ErrorOnSuspendSwitch()).toHaveValue('1');
-    });
-    it('set errorOnSuspend to false', async () => {
         await screen.Apply({ errorOnSuspend: false });
         await expect(screen.ErrorOnSuspendSwitch()).toHaveValue('0');
     });
-    it('set errorOnResume to true', async () => {
+    it('switch errorOnResume on and off', async () => {
         await screen.Apply({ errorOnResume: true });
         await expect(screen.ErrorOnResumeSwitch()).toHaveValue('1');
-    });
-    it('set errorOnResume to false', async () => {
         await screen.Apply({ errorOnResume: false });
         await expect(screen.ErrorOnResumeSwitch()).toHaveValue('0');
     });
