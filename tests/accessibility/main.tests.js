5cb60d3af094a32426d64726ddffa7ad510bc9f9
Jamie <jamie.h.bate@gmail.com>
2020-10-27 19:40:15 +1300

    try all tests

--- tests/accessibility/main.tests.js
+++ tests/accessibility/main.tests.js
@@ -1,151 +1,151 @@
 module.exports = (test) => {
-    // describe('Status Screen', () => {
-    //     var statusScreen;
-    //     it('has a Active Carbohydrates Label', async () => {
-    //         statusScreen = await test.OpenStatusScreen();
-    //     });
-    //     it('has a Active Carbohydrates Label', async () => {
-    //         await expect(statusScreen.ActiveCarbohydratesLabel).toBeVisible();
-    //     });
-    //     it('has a Active Insulin Label', async () => {
-    //         await expect(statusScreen.ActiveInsulinLabel).toBeVisible();
-    //     });
-    //     it('has a Insulin Delivery Label', async () => {
-    //         await expect(statusScreen.InsulinDeliveryLabel).toBeVisible();
-    //     });
-    //     it('has a Glucose Label', async () => {
-    //         await expect(statusScreen.GlucoseLabel).toBeVisible();
-    //     });
-    //     it('has a Settings Button', async () => {
-    //         await expect(statusScreen.SettingsButton).toBeVisible();
-    //     });
-    //     it('has a Add Meal Button', async () => {
-    //         await expect(statusScreen.AddMealButton).toBeVisible();
-    //     });
-    //     it('has a Bolus Button', async () => {
-    //         await expect(statusScreen.BolusButton).toBeVisible();
-    //     });
-    //     describe('HUD', () => {
-    //         it('add pump button', async () => {
-    //             await expect(statusScreen.HeaderSection.Devices.AddPumpButton()).toBeVisible();
-    //         });
-    //         it('add CGM button', async () => {
-    //             await expect(statusScreen.HeaderSection.Devices.AddCGMButton()).toBeVisible();
-    //         });
-    //         it('Loop button', async () => {
-    //             await expect(statusScreen.HeaderSection.LoopIcon).toBeVisible();
-    //         });
-    //         it('Tap to add blood glucose button', async () => {
-    //             await expect(statusScreen.HeaderSection.EnterBloodGlucoseButton).toBeVisible();
-    //         });
-    //         it('No recent blood glucose label', async () => {
-    //             await expect(statusScreen.HeaderSection.NoRecentBloodGlucoseLabel).toBeVisible();
-    //         });
-    //     });
-    // });
-    // describe('Insulin Delivery Chart', () => {
-    //     var insulinDeliveryChart;
-    //     beforeAll(async () => {
-    //         let statusScreen = await test.OpenStatusScreen();
-    //         insulinDeliveryChart = await statusScreen.OpenInsulinDeliveryChart();
-    //     });
-    //     it('has a header', async () => {
-    //         await expect(insulinDeliveryChart.Header).toBeVisible();
-    //     });
-    //     it('has a IOB Label', async () => {
-    //         await expect(insulinDeliveryChart.IOBLabel).toBeVisible();
-    //     });
-    //     it('has a Total Label', async () => {
-    //         await expect(insulinDeliveryChart.TotalLabel).toBeVisible();
-    //     });
-    //     it('has an Event History Label', async () => {
-    //         await expect(insulinDeliveryChart.EventHistoryLabel).toBeVisible();
-    //     });
-    //     it('has a Reservoir Label', async () => {
-    //         await expect(insulinDeliveryChart.ReservoirLabel).toBeVisible();
-    //     });
-    //     it('has a Back Button', async () => {
-    //         await insulinDeliveryChart.BackButton.tap();
-    //     });
-    // });
-    // describe('Active Carbohydrates Chart', () => {
-    //     let activeCarbohydratesChart;
-    //     beforeAll(async () => {
-    //         let statusScreen = await test.OpenStatusScreen();
-    //         activeCarbohydratesChart = await statusScreen.OpenActiveCarbohydratesChart();
-    //     });
-    //     it('has a header', async () => {
-    //         await expect(activeCarbohydratesChart.Header).toBeVisible();
-    //     });
-    //     it('has a Grams Active Carbs Label', async () => {
-    //         await expect(activeCarbohydratesChart.GramsActiveCarbsLabel).toBeVisible();
-    //     });
-    //     it('has a Grams Total Carbs Label', async () => {
-    //         await expect(activeCarbohydratesChart.GramsTotalCarbsLabel).toBeVisible();
-    //     });
-    //     it('has an Glucose Change Label', async () => {
-    //         await expect(activeCarbohydratesChart.GlucoseChangeLabel).toBeVisible();
-    //     });
-    //     it('has a Observed Label', async () => {
-    //         await expect(activeCarbohydratesChart.ObservedLabel).toBeVisible();
-    //     });
-    //     it('has a Predicted Label', async () => {
-    //         await expect(activeCarbohydratesChart.PredictedLabel).toBeVisible();
-    //     });
-    //     it('has a Back Button', async () => {
-    //         await activeCarbohydratesChart.BackButton.tap();
-    //     });
-    // });
-    // describe('Active Insulin Chart', () => {
-    //     let activeInsulinChart;
-    //     beforeAll(async () => {
-    //         let statusScreen = await test.OpenStatusScreen();
-    //         activeInsulinChart = await statusScreen.OpenActiveInsulinChart();
-    //     });
-    //     it('has a header', async () => {
-    //         await expect(activeInsulinChart.Header).toBeVisible();
-    //     });
-    //     it('has a IOB Label', async () => {
-    //         await expect(activeInsulinChart.IOBLabel).toBeVisible();
-    //     });
-    //     it('has a Total Label', async () => {
-    //         await expect(activeInsulinChart.TotalLabel).toBeVisible();
-    //     });
-    //     it('has an Event History Label', async () => {
-    //         await expect(activeInsulinChart.EventHistoryLabel).toBeVisible();
-    //     });
-    //     it('has a Reservoir Label', async () => {
-    //         await expect(activeInsulinChart.ReservoirLabel).toBeVisible();
-    //     });
-    //     it('has a Back Button', async () => {
-    //         await activeInsulinChart.BackButton.tap();
-    //     });
-    // });
-    // describe('Glucose Chart', () => {
-    //     let glucoseChart;
-    //     beforeAll(async () => {
-    //         let statusScreen = await test.OpenStatusScreen();
-    //         glucoseChart = await statusScreen.OpenGlucoseChart();
-    //     });
-    //     it('has a header', async () => {
-    //         await expect(glucoseChart.Header).toBeVisible();
-    //     });
-    //     it('has a Carbohydrates Label', async () => {
-    //         await expect(glucoseChart.CarbohydratesLabel).toBeVisible();
-    //     });
-    //     it('has a Insulin Label', async () => {
-    //         await expect(glucoseChart.InsulinLabel).toBeVisible();
-    //     });
-    //     it('has a Glucose Momentum Label', async () => {
-    //         await expect(glucoseChart.GlucoseMomentumLabel).toBeVisible();
-    //     });
-    //     it('has a Retrospective Correction Label', async () => {
-    //         await expect(glucoseChart.RetrospectiveCorrectionLabel).toBeVisible();
-    //     });
-    //     it('has a Back Button', async () => {
-    //         await glucoseChart.BackButton.tap();
-    //     });
-    // });
+    describe('Status Screen', () => {
+        var statusScreen;
+        it('has a Active Carbohydrates Label', async () => {
+            statusScreen = await test.OpenStatusScreen();
+        });
+        it('has a Active Carbohydrates Label', async () => {
+            await expect(statusScreen.ActiveCarbohydratesLabel).toBeVisible();
+        });
+        it('has a Active Insulin Label', async () => {
+            await expect(statusScreen.ActiveInsulinLabel).toBeVisible();
+        });
+        it('has a Insulin Delivery Label', async () => {
+            await expect(statusScreen.InsulinDeliveryLabel).toBeVisible();
+        });
+        it('has a Glucose Label', async () => {
+            await expect(statusScreen.GlucoseLabel).toBeVisible();
+        });
+        it('has a Settings Button', async () => {
+            await expect(statusScreen.SettingsButton).toBeVisible();
+        });
+        it('has a Add Meal Button', async () => {
+            await expect(statusScreen.AddMealButton).toBeVisible();
+        });
+        it('has a Bolus Button', async () => {
+            await expect(statusScreen.BolusButton).toBeVisible();
+        });
+        describe('HUD', () => {
+            it('add pump button', async () => {
+                await expect(statusScreen.HeaderSection.Devices.AddPumpButton()).toBeVisible();
+            });
+            it('add CGM button', async () => {
+                await expect(statusScreen.HeaderSection.Devices.AddCGMButton()).toBeVisible();
+            });
+            it('Loop button', async () => {
+                await expect(statusScreen.HeaderSection.LoopIcon).toBeVisible();
+            });
+            it('Tap to add blood glucose button', async () => {
+                await expect(statusScreen.HeaderSection.EnterBloodGlucoseButton).toBeVisible();
+            });
+            it('No recent blood glucose label', async () => {
+                await expect(statusScreen.HeaderSection.NoRecentBloodGlucoseLabel).toBeVisible();
+            });
+        });
+    });
+    describe('Insulin Delivery Chart', () => {
+        var insulinDeliveryChart;
+        beforeAll(async () => {
+            let statusScreen = await test.OpenStatusScreen();
+            insulinDeliveryChart = await statusScreen.OpenInsulinDeliveryChart();
+        });
+        it('has a header', async () => {
+            await expect(insulinDeliveryChart.Header).toBeVisible();
+        });
+        it('has a IOB Label', async () => {
+            await expect(insulinDeliveryChart.IOBLabel).toBeVisible();
+        });
+        it('has a Total Label', async () => {
+            await expect(insulinDeliveryChart.TotalLabel).toBeVisible();
+        });
+        it('has an Event History Label', async () => {
+            await expect(insulinDeliveryChart.EventHistoryLabel).toBeVisible();
+        });
+        it('has a Reservoir Label', async () => {
+            await expect(insulinDeliveryChart.ReservoirLabel).toBeVisible();
+        });
+        it('has a Back Button', async () => {
+            await insulinDeliveryChart.BackButton.tap();
+        });
+    });
+    describe('Active Carbohydrates Chart', () => {
+        let activeCarbohydratesChart;
+        beforeAll(async () => {
+            let statusScreen = await test.OpenStatusScreen();
+            activeCarbohydratesChart = await statusScreen.OpenActiveCarbohydratesChart();
+        });
+        it('has a header', async () => {
+            await expect(activeCarbohydratesChart.Header).toBeVisible();
+        });
+        it('has a Grams Active Carbs Label', async () => {
+            await expect(activeCarbohydratesChart.GramsActiveCarbsLabel).toBeVisible();
+        });
+        it('has a Grams Total Carbs Label', async () => {
+            await expect(activeCarbohydratesChart.GramsTotalCarbsLabel).toBeVisible();
+        });
+        it('has an Glucose Change Label', async () => {
+            await expect(activeCarbohydratesChart.GlucoseChangeLabel).toBeVisible();
+        });
+        it('has a Observed Label', async () => {
+            await expect(activeCarbohydratesChart.ObservedLabel).toBeVisible();
+        });
+        it('has a Predicted Label', async () => {
+            await expect(activeCarbohydratesChart.PredictedLabel).toBeVisible();
+        });
+        it('has a Back Button', async () => {
+            await activeCarbohydratesChart.BackButton.tap();
+        });
+    });
+    describe('Active Insulin Chart', () => {
+        let activeInsulinChart;
+        beforeAll(async () => {
+            let statusScreen = await test.OpenStatusScreen();
+            activeInsulinChart = await statusScreen.OpenActiveInsulinChart();
+        });
+        it('has a header', async () => {
+            await expect(activeInsulinChart.Header).toBeVisible();
+        });
+        it('has a IOB Label', async () => {
+            await expect(activeInsulinChart.IOBLabel).toBeVisible();
+        });
+        it('has a Total Label', async () => {
+            await expect(activeInsulinChart.TotalLabel).toBeVisible();
+        });
+        it('has an Event History Label', async () => {
+            await expect(activeInsulinChart.EventHistoryLabel).toBeVisible();
+        });
+        it('has a Reservoir Label', async () => {
+            await expect(activeInsulinChart.ReservoirLabel).toBeVisible();
+        });
+        it('has a Back Button', async () => {
+            await activeInsulinChart.BackButton.tap();
+        });
+    });
+    describe('Glucose Chart', () => {
+        let glucoseChart;
+        beforeAll(async () => {
+            let statusScreen = await test.OpenStatusScreen();
+            glucoseChart = await statusScreen.OpenGlucoseChart();
+        });
+        it('has a header', async () => {
+            await expect(glucoseChart.Header).toBeVisible();
+        });
+        it('has a Carbohydrates Label', async () => {
+            await expect(glucoseChart.CarbohydratesLabel).toBeVisible();
+        });
+        it('has a Insulin Label', async () => {
+            await expect(glucoseChart.InsulinLabel).toBeVisible();
+        });
+        it('has a Glucose Momentum Label', async () => {
+            await expect(glucoseChart.GlucoseMomentumLabel).toBeVisible();
+        });
+        it('has a Retrospective Correction Label', async () => {
+            await expect(glucoseChart.RetrospectiveCorrectionLabel).toBeVisible();
+        });
+        it('has a Back Button', async () => {
+            await glucoseChart.BackButton.tap();
+        });
+    });
     describe('Bolus Screen', () => {
         let bolusScreen;
         it('has to open the bolus screen', async () => {
