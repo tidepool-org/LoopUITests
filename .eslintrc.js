5df2cc4cddbbc24815e97d7c59f15c124208aeac
Ginny Yadav <ginny@tidepool.org>
2021-02-19 13:33:18 -0500

    turning off some rules we don't need

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
-{
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "jest": true
-    },
-    "globals": {
-        "device": "off"
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {}
-}
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'max-len': 'off',
+    'jest/expect-expect': 'off',
+    'no-await-in-loop': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-throw-literal': 'off',
+    'jest/no-standalone-expect': 'off',
+    'no-console': 'off',
+  },
+};
